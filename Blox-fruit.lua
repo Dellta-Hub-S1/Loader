503a38cc292cbaea2326d7c1479ad239ab6bd5ab
Dellta-Hub-S1 <93393225+Dellta-Hub-S1@users.noreply.github.com>
2022-01-14 10:56:18 +0700

    Update Blox-fruit.lua

--- Blox-fruit.lua
+++ Blox-fruit.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Dellta-Hub-S1/BF/main/Loder.cf"))();
   elseif game.PlaceId == 286090429 then
-  loadstring(game:HttpGet"https://raw.githubusercontent.com/Dellta-Hub-S1/Asenal-/main/.lua")
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Dellta-Hub-S1/Asenal-/main/.lua"))();
   end
